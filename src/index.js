560c8914d71dc92e6b524367c2b7fc793db2f361
Alan Campo <acampos@netatlantic.com>
2018-09-14 15:37:45 -0400

    Created threew previes and added no white space wrap to the shelf icon

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Shelf from './components/Shelf';
+import Row from './components/Row';
+import Column from './components/Column';
 
 ReactDOM.render(
-   <Shelf featuredIconId='iconId' featuredBodyId='bodyId'></Shelf>,
+    <Row>
+        <Column size={4}>
+            <Shelf featuredIconId='iconId' featuredBodyId='bodyId'>Children</Shelf>
+        </Column> <Column size={4}>
+            <Shelf featuredIconId='iconId' featuredBodyId='bodyId'>Children</Shelf>
+        </Column> <Column size={4}>
+            <Shelf featuredIconId='iconId' featuredBodyId='bodyId'>Children</Shelf>
+        </Column>
+    </Row>,
     document.getElementById('root'),
 );
 registerServiceWorker();
