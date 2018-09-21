2f81bda2282c61d2dd353fa4b608c6bea9e950f6
Alan Campo <acampos@netatlantic.com>
2018-09-21 16:10:28 -0400

    created label component

--- src/index.js
+++ src/index.js
@@ -9,15 +9,34 @@ import Column from './components/Column';
 ReactDOM.render(
   <Row>
     <Column size={4}>
-      <Shelf title="StrongView Title" iconText="SV" featuredIconId="iconId" featuredBodyId="bodyId">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Shelf>
+      <Shelf
+        title="StrongView Title"
+        iconText="SV"
+        featuredIconId="iconId"
+        featuredBodyId="bodyId"
+      >
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+      </Shelf>
     </Column>
-    {' '}
     <Column size={4}>
-      <Shelf title="ListManager Title" iconText="LM" featuredIconId="iconId" featuredBodyId="bodyId">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Shelf>
+      <Shelf
+        title="ListManager Title"
+        iconText="LM"
+        featuredIconId="iconId"
+        featuredBodyId="bodyId"
+      >
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+      </Shelf>
     </Column>
-    {' '}
     <Column size={4}>
-      <Shelf title="NextMail Title" iconText="NM" featuredIconId="iconId" featuredBodyId="bodyId">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Shelf>
+      <Shelf
+        title="NextMail Title"
+        iconText="NM"
+        featuredIconId="iconId"
+        featuredBodyId="bodyId"
+      >
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+      </Shelf>
     </Column>
   </Row>,
   document.getElementById('root'),
