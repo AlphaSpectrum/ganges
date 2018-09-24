2c0e94cd50ad8562461436cb387b42ccc7667a3b
Alan Campo <acampos@netatlantic.com>
2018-09-24 09:45:36 -0400

    added button component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Label from './components/Label';
 import Input from './components/Input';
+import Button from './components/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
@@ -11,6 +12,7 @@ ReactDOM.render(
       {'Label'}
       <Input id="exampleInput" type="text" />
     </Label>
+    <Button id='buttonExample'>Submit</Button>
   </div>,
   document.getElementById('root'),
 );
