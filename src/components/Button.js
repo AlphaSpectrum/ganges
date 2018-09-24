ff59717c49c5f9b5f84b9ca40f1e9661f8cd88f2
Alan Campo <acampos@netatlantic.com>
2018-09-24 09:44:33 -0400

    added onClick event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ id, className, children }) => (
-  <button id={id} type="button" className={`btn ${className}`}>{children}</button>
+const Button = ({
+  id, className, children, onClick,
+}) => (
+  <button id={id} type="button" className={`btn ${className}`} onClick={onClick}>{children}</button>
 );
 
 Button.propTypes = {
   id: PropTypes.string.isRequired,
   children: PropTypes.children,
   className: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 Button.defaultTypes = {
