621bafe385a8849a058f9708a0d647431b8c535b
Alan Campo <acampos@netatlantic.com>
2018-09-24 10:26:47 -0400

    Ran eslint

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Textarea = ({ id, rows, ...otherProps }) => (
-  <textarea className="form-control" id={id} rows={rows} {...otherProps} />
+const Textarea = ({
+  id, rows, onChange, ...otherProps
+}) => (
+  <textarea className="form-control" id={id} rows={rows} onChange={onChange} {...otherProps} />
 );
 
 Textarea.propTypes = {
