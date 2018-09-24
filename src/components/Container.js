0f967e7ca1d637d7aa69c80c17d136480945292e
Alan Campo <acampos@netatlantic.com>
2018-09-24 13:27:00 -0400

    Added a -fluid toggler for the class "container"

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Container = ({ children, ...otherProps }) => (<div className="container" {...otherProps}>{children}</div>);
+const Container = ({ children, fluid, ...otherProps }) => {
+  if (fluid === null) {
+    return (<div className="container" {...otherProps}>{children}</div>);
+  }
+  return (<div className="container-fluid" {...otherProps}>{children}</div>);
+};
 
 export default Container;
