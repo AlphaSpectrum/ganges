251d24f6f7f6ba18d20c3231f36753676bcaef43
Alan Campo <acampos@netatlantic.com>
2018-09-26 09:19:22 -0400

    Created the range slider component

---
+++ src/components/RangeSlider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Label from './Label';
+
+const RangeSlider = ({}) => {
+    return(
+        <Label>Test</Label>
+    );
+}
+
+export default RangeSlider;
