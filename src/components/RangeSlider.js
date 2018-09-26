92f7ffaf88b1c2d33a09b978b0b98252ffddaa44
Alan Campo <acampos@netatlantic.com>
2018-09-26 10:38:45 -0400

    added props validation

--- src/components/RangeSlider.js
+++ src/components/RangeSlider.js
@@ -9,4 +9,20 @@ const RangeSlider = ({
   </div>
 );
 
+RangeSlider.propTypes = {
+  id: PropTypes.string,
+  max: PropTypes.number,
+  min: PropTypes.number,
+  step: PropTypes.number,
+  value: PropTypes.number,
+};
+
+RangeSlider.defaultProps = {
+  id: null,
+  max: 10,
+  min: 0,
+  step: 1,
+  value: null,
+};
+
 export default RangeSlider;
